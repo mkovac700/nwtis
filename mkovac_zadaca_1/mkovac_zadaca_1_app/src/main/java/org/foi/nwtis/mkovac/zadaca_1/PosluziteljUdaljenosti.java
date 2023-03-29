bc8c6a6f5ee5092537c12f0488480fae2e6c548e
Marijan Kovac <mkovac@foi.hr>
2023-03-29 23:00:47 +0200

    Zadaća 1 - samostalno #3

--- mkovac_zadaca_1/mkovac_zadaca_1_app/src/main/java/org/foi/nwtis/mkovac/zadaca_1/PosluziteljUdaljenosti.java
+++ mkovac_zadaca_1/mkovac_zadaca_1_app/src/main/java/org/foi/nwtis/mkovac/zadaca_1/PosluziteljUdaljenosti.java
@@ -2,12 +2,19 @@ package org.foi.nwtis.mkovac.zadaca_1;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,16 +27,20 @@ import org.foi.nwtis.mkovac.zadaca_1.podaci.Udaljenost;
 
 public class PosluziteljUdaljenosti {
 
-  private int mreznaVrata;
-  private int brojCekaca;
+  private int mreznaVrata = 8000;
+  private int brojCekaca = 0;
+  private int brojZadnjihSpremljenih = 0;
+  private String datotekaSerijalizacija = "";
 
-  private List<Udaljenost> listaUdaljenosti;
+  private final double r = 6371;
+
+  private List<Udaljenost> listaUdaljenosti = new ArrayList<>();
 
   public static void main(String[] args) {
     var pu = new PosluziteljUdaljenosti();
 
     if (!PosluziteljUdaljenosti.provjeriArgumente(args)) {
-      Logger.getLogger(PokretacPosluzitelja.class.getName()).log(Level.SEVERE,
+      Logger.getGlobal().log(Level.SEVERE,
           "Nije upisan naziv datoteke ili datoteka nema odgovarajući format!");
       return;
     }
@@ -67,14 +78,61 @@ public class PosluziteljUdaljenosti {
     // ucitat postavke
     mreznaVrata = Integer.parseInt(konf.dajPostavku("mreznaVrata"));
     brojCekaca = Integer.parseInt(konf.dajPostavku("brojCekaca"));
+    brojZadnjihSpremljenih = Integer.parseInt(konf.dajPostavku("brojZadnjihSpremljenih"));
+    datotekaSerijalizacija = konf.dajPostavku("datotekaSerijalizacija");
+
+    // pripremit dat serial
+    try {
+      ucitajDatotekuSerijalizacija(datotekaSerijalizacija);
+    } catch (IOException | ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
     // otvorit port
     otvoriMreznaVrata();
   }
 
+  private void ucitajDatotekuSerijalizacija(String nazivDatoteke)
+      throws IOException, ClassNotFoundException {
+    var putanja = Path.of(nazivDatoteke);
+
+    if (Files.exists(putanja) && (Files.isDirectory(putanja) || !Files.isReadable(putanja))) {
+      throw new IOException(
+          "Datoteka '" + nazivDatoteke + "' nije datoteka ili nije moguće otvoriti!");
+    }
+
+    if (putanja.toFile().length() != 0) {
+      ObjectInputStream citac = new ObjectInputStream(new FileInputStream(putanja.toFile()));
+
+      Object o;
+      while ((o = citac.readObject()) != null)
+        listaUdaljenosti = (List<Udaljenost>) o;
+
+      citac.close();
+    }
+
+  }
+
+  private void spremiDatotekuSerijalizacija(String nazivDatoteke) throws IOException {
+    var putanja = Path.of(nazivDatoteke);
+
+    if (!Files.exists(putanja) || Files.isDirectory(putanja) || !Files.isWritable(putanja)) {
+      throw new IOException(
+          "Datoteka '" + nazivDatoteke + "' nije datoteka ili nije moguće otvoriti!");
+    }
+
+    ObjectOutputStream pisac = new ObjectOutputStream(new FileOutputStream(putanja.toFile()));
+
+    pisac.writeObject(this.listaUdaljenosti);
+
+    pisac.close();
+  }
+
   private void otvoriMreznaVrata() {
     try (ServerSocket posluzitelj = new ServerSocket(this.mreznaVrata, this.brojCekaca)) {
       while (true) {
+        System.out.println("čekam zahtjev...");
         Socket uticnica = posluzitelj.accept(); // program stoji i ceka da dode zahtjev
         /*
          * var dretva = new MrezniRadnik(uticnica, konfig); dretva.start();
@@ -94,9 +152,15 @@ public class PosluziteljUdaljenosti {
           zahtjev.append(red);
         }
 
-        obradiZahtjev(zahtjev.toString());
+        uticnica.shutdownInput();
 
+        String odgovor = obradiZahtjev(zahtjev.toString());
 
+        pisac.write(odgovor);
+        pisac.flush();
+
+        uticnica.shutdownOutput();
+        uticnica.close();
 
       }
     } catch (IOException e) {
@@ -109,7 +173,7 @@ public class PosluziteljUdaljenosti {
     String regex1 =
         "UDALJENOST ([(\\d*\\.)?\\d+]) ([(\\d*\\.)?\\d+]) ([(\\d*\\.)?\\d+]) ([(\\d*\\.)?\\d+])$";
 
-    String regex2 = "";
+    String regex2 = "UDALJENOST SPREMI$";
 
     if (provjeriIzraz(zahtjev, regex1)) { // UDALJENOST 46.30771 16.33808 46.02419 15.90968
       var podaci = zahtjev.split(" "); // podaci[0] komanda
@@ -130,36 +194,44 @@ public class PosluziteljUdaljenosti {
       if (postoji) {
         return "OK " + udaljenost;
       } else { // izracunaj udaljenost
-        double gpsSirina1 = Double.parseDouble(podaci[1]);
-        double gpsDuzina1 = Double.parseDouble(podaci[2]);
-        double gpsSirina2 = Double.parseDouble(podaci[3]);
-        double gpsDuzina2 = Double.parseDouble(podaci[4]);
+        double rezultat = izracunajUdaljenost(podaci);
+        if (listaUdaljenosti.size() == brojZadnjihSpremljenih)
+          listaUdaljenosti.remove(brojZadnjihSpremljenih);
+        listaUdaljenosti.add(
+            new Udaljenost(podaci[1], podaci[2], podaci[3], podaci[4], Double.toString(rezultat)));
 
-        gpsSirina1 = Math.toRadians(gpsSirina1); // latitude = sirina
-        gpsDuzina1 = Math.toRadians(gpsDuzina1); // longitude = duzina
-        gpsSirina2 = Math.toRadians(gpsSirina2);
-        gpsDuzina2 = Math.toRadians(gpsDuzina2);
-
-        double sirina = gpsSirina2 - gpsSirina1; // lat
-        double duzina = gpsDuzina2 + gpsDuzina1; // lon
-
-        double a = Math.pow(Math.sin(sirina) / 2, 2)
-            + Math.cos(gpsSirina1) * Math.cos(gpsSirina2) * Math.pow(Math.sin(duzina / 2), 2);
-
-        double c = 2 * Math.asin(Math.sqrt(a));
-
-        double r = 6371;
-
-        double rezultat = c * r;
-
-        return "OK " + rezultat;
+        return "OK " + String.format("%.2f", rezultat);
       }
 
     } else if (provjeriIzraz(zahtjev, regex2)) { // UDALJENOST SPREMI
-      return "";
-    } else { // neispravno; vrati ERROR 10
-      return "ERROR 10";
+      try {
+        spremiDatotekuSerijalizacija(datotekaSerijalizacija);
+      } catch (IOException e) {
+        Logger.getGlobal().log(Level.SEVERE, e.getMessage());
+        return "ERROR 19 Greska u spremanju podataka u datoteku";
+      }
+      return "OK";
+    } else {
+      return "ERROR 10 Format komande nije ispravan";
     }
+  }
+
+  private double izracunajUdaljenost(String[] podaci) {
+
+    double gpsSirina1 = Math.toRadians(Double.parseDouble(podaci[1])); // latitude = sirina
+    double gpsDuzina1 = Math.toRadians(Double.parseDouble(podaci[2])); // longitude = duzina
+    double gpsSirina2 = Math.toRadians(Double.parseDouble(podaci[3]));
+    double gpsDuzina2 = Math.toRadians(Double.parseDouble(podaci[4]));
+
+    double sirina = gpsSirina2 - gpsSirina1; // lat
+    double duzina = gpsDuzina2 + gpsDuzina1; // lon
+
+    double a = Math.pow(Math.sin(sirina) / 2, 2)
+        + Math.cos(gpsSirina1) * Math.cos(gpsSirina2) * Math.pow(Math.sin(duzina / 2), 2);
+
+    double c = 2 * Math.asin(Math.sqrt(a));
+
+    return c * r;
   }
 
   private boolean provjeriIzraz(String string, String regex) {
--- mkovac_zadaca_1/mkovac_zadaca_1_app/src/main/java/org/foi/nwtis/mkovac/zadaca_1/podaci/Udaljenost.java
+++ mkovac_zadaca_1/mkovac_zadaca_1_app/src/main/java/org/foi/nwtis/mkovac/zadaca_1/podaci/Udaljenost.java
@@ -1,6 +1,8 @@
 package org.foi.nwtis.mkovac.zadaca_1.podaci;
 
+import java.io.Serializable;
+
 public record Udaljenost(String gpsSirina1, String gpsDuzina1, String gpsSirina2, String gpsDuzina2,
-    String udaljenost) {
+    String udaljenost) implements Serializable {
 
 }
