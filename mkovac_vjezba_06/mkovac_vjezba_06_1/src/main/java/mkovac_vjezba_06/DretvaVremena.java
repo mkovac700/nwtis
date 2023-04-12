324336e7bb292f5080ab50106f4a5c932b324d28
Marijan Kovac <mkovac@foi.hr>
2023-04-12 10:54:54 +0200

    Vježba 06 - treći dio

---
+++ mkovac_vjezba_06/mkovac_vjezba_06_1/src/main/java/mkovac_vjezba_06/DretvaVremena.java
@@ -0,0 +1,52 @@
+package mkovac_vjezba_06;
+
+public class DretvaVremena extends Thread {
+  private static int brojDretve = 0;
+  private int brojCiklusa;
+  private int trajanjeCiklusa;
+  private boolean kraj = false;
+
+  public DretvaVremena(int brojCiklusa, int trajanjeCiklusa) {
+    super("mkovac-" + brojDretve++);
+    this.brojCiklusa = brojCiklusa;
+    this.trajanjeCiklusa = trajanjeCiklusa;
+  }
+
+  @Override
+  public void start() {
+    super.start();
+  }
+
+  @Override
+  public void run() {
+    int brojac = 0;
+    /*
+     * try { while (brojac < this.brojCiklusa) { System.out.println(
+     * "Dretva: " + this.getName() + " brojac " + brojac++);
+     * 
+     * Thread.sleep(trajanjeCiklusa); } } catch (InterruptedException
+     * e) { e.printStackTrace(); }
+     */
+
+
+    while (brojac < this.brojCiklusa && !this.kraj) {
+      System.out.println(
+          "Dretva: " + this.getName() + " brojac " + brojac++);
+      try {
+        Thread.sleep(trajanjeCiklusa);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+
+  }
+
+  @Override
+  public void interrupt() {
+    this.kraj = true;
+    super.interrupt();
+  }
+
+
+}
---
+++ mkovac_vjezba_06/mkovac_vjezba_06_1/src/main/java/mkovac_vjezba_06/Vjezba_06_3.java
@@ -0,0 +1,49 @@
+package mkovac_vjezba_06;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Vjezba_06_3", urlPatterns = {"/Vjezba_06_3"})
+public class Vjezba_06_3 extends HttpServlet {
+
+  private static final long serialVersionUID = -25077235507969930L;
+  // private DretvaVremena dv; //ne mozemo ovako jer na kreiranje vise
+  // dretvi na interrupt se gasi samo zadnja jer se ostale prepisu u
+  // memoriji
+  List<DretvaVremena> dretve = new ArrayList<DretvaVremena>();
+
+  protected void doGet(HttpServletRequest request,
+      HttpServletResponse response)
+      throws ServletException, IOException {
+
+    int brojCiklusa =
+        Integer.parseInt(request.getParameter("brojCiklusa"));
+    int trajanjeCiklusa =
+        Integer.parseInt(request.getParameter("trajanjeCiklusa"));
+
+    DretvaVremena dv =
+        new DretvaVremena(brojCiklusa, trajanjeCiklusa);
+    this.dretve.add(dv);
+    dv.start();
+  }
+
+  @Override
+  public void destroy() {
+    // TODO tu ide sve sto se misli napravit prije nego se ugasi
+    // servlet
+    for (DretvaVremena d : this.dretve) {
+      if (d != null && d.isAlive()) // da se ne interrupta ako ne
+                                    // postoji tj ako je vec gotova
+        d.interrupt();
+    }
+    super.destroy();
+  }
+
+
+}
