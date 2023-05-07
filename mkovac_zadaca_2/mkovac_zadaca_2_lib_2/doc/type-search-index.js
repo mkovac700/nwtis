c2b24ddf105e2d08bd80df10780bba24c5166820
Marijan Kovac <mkovac@foi.hr>
2023-05-07 16:00:59 +0200

    Zadaća 2 - samostalno #19
    Napomena: Ispravci wa_1, izrada dokumentacije za lib_1, lib_2, wa_1

---
+++ mkovac_zadaca_2/mkovac_zadaca_2_lib_2/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.foi.nwtis","l":"KonfiguracijaBP"},{"p":"org.foi.nwtis","l":"PostavkeBazaPodataka"}];updateSearchResults();
