c42086efec8fc6a97e6633a4de407b45dcc86d8d
Marijan Kovac <mkovac@foi.hr>
2023-06-08 18:13:36 +0200

    Projekt #2

--- mkovac-projekt/mkovac_aplikacija_1/src/main/java/org/foi/nwtis/mkovac/aplikacija_1/GlavniPosluzitelj.java
+++ mkovac-projekt/mkovac_aplikacija_1/src/main/java/org/foi/nwtis/mkovac/aplikacija_1/GlavniPosluzitelj.java
@@ -1,15 +1,32 @@
 package org.foi.nwtis.mkovac.aplikacija_1;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.foi.nwtis.Konfiguracija;
-import lombok.Setter;
 
 public class GlavniPosluzitelj {
   private Konfiguracija konfig;
 
-  @Setter
-  private boolean status;
+  private AtomicBoolean status;
+
+  private AtomicBoolean kraj;
+
+  private AtomicBoolean ispis;
+
+  private AtomicInteger brojacUdaljenosti;
+
+  private int mreznaVrata = 8000;
+  private int brojRadnika = 10;
+  // private int maksCekanje = 5000;
+  private int brojCekaca = 5;
 
   /**
    * Konstruktor za GlavniPosluzitelj
@@ -18,31 +35,74 @@ public class GlavniPosluzitelj {
    */
   public GlavniPosluzitelj(Konfiguracija konfig) {
     this.konfig = konfig;
+
+    status = new AtomicBoolean(false);
+    kraj = new AtomicBoolean(false);
+    ispis = new AtomicBoolean(false);
+    brojacUdaljenosti = new AtomicInteger(0);
   }
 
   public void pokreniPosluzitelja() {
+
+    try {
+      mreznaVrata = Integer.parseInt(konfig.dajPostavku("mreznaVrata"));
+      brojRadnika = Integer.parseInt(konfig.dajPostavku("brojRadnika"));
+      // maksCekanje = Integer.parseInt(konfig.dajPostavku("maksCekanje"));
+      brojCekaca = Integer.parseInt(konfig.dajPostavku("brojCekaca"));
+    } catch (NumberFormatException e) {
+      Logger.getGlobal().log(Level.SEVERE, "Postavke nisu ispravne! " + e.getMessage()
+          + " Nastavljam rad sa zadanim vrijednostima...");
+    }
+
     otvoriMreznaVrata();
   }
 
   private void otvoriMreznaVrata() {
-    // try (var posluzitelj = new ServerSocket(0, 0)) {
-    // var dretva = new MrezniRadnik(this::setStatus); // lombok
-    // } catch (IOException e) {
-    // Logger.getGlobal().log(Level.SEVERE, e.getMessage());
-    // }
-    status = false;
+    List<Thread> dretve = new ArrayList<>();
 
-    System.out.println("status na pocetku: " + status);
+    try (var posluzitelj = new ServerSocket(mreznaVrata, brojCekaca)) {
 
-    ExecutorService executor = Executors.newFixedThreadPool(5);
-    var dretva = new MrezniRadnik(this::setStatus);
-    executor.execute(dretva);
+      ExecutorService executor = Executors.newFixedThreadPool(brojRadnika);
 
-    executor.shutdown();
-    while (!executor.isTerminated());
+      while (!this.kraj.get()) {
+        var uticnica = posluzitelj.accept();
 
-    System.out.println("status nakon azuriranja: " + status);
-    System.out.println("kraj rada");
+        var mrezniRadnik =
+            new MrezniRadnik(uticnica, this.status, this.kraj, this.ispis, this.brojacUdaljenosti);
+        Thread dretva = new Thread(mrezniRadnik);
+
+        executor.execute(dretva);
+
+        dretve.add(dretva);
+      }
+
+      posluzitelj.close();
+
+      // dodati interruptiranje i gasenje executora
+
+      executor.shutdown(); // ili shutdownNow za INTERRUPT!!
+      while (!executor.isTerminated());
+
+      Logger.getGlobal().log(Level.INFO, "Glavni poslužitelj - kraj rada");
+
+    } catch (IOException e) {
+      Logger.getGlobal().log(Level.SEVERE, e.getMessage());
+    }
+
+
+
+    // System.out.println("status na pocetku: " + status);
+    //
+    // ExecutorService executor = Executors.newFixedThreadPool(5);
+    // var dretva = new MrezniRadnik(this::setStatus);
+    // executor.execute(dretva);
+    //
+    // executor.shutdown();
+    // while (!executor.isTerminated());
+    //
+    // System.out.println("status nakon azuriranja: " + status);
+    // System.out.println("kraj rada");
+
   }
 
 
--- mkovac-projekt/mkovac_aplikacija_1/src/main/java/org/foi/nwtis/mkovac/aplikacija_1/MrezniRadnik.java
+++ mkovac-projekt/mkovac_aplikacija_1/src/main/java/org/foi/nwtis/mkovac/aplikacija_1/MrezniRadnik.java
@@ -1,18 +1,195 @@
 package org.foi.nwtis.mkovac.aplikacija_1;
 
-import java.util.function.Consumer;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MrezniRadnik implements Runnable {
-  private Consumer<Boolean> azurirajStatus;
+  private Socket uticnica;
 
-  public MrezniRadnik(Consumer<Boolean> azurirajStatus) {
-    this.azurirajStatus = azurirajStatus;
+  private AtomicBoolean status;
+  private AtomicBoolean ispis;
+  private AtomicBoolean kraj;
+  private AtomicInteger brojacUdaljenosti;
+
+  public MrezniRadnik(Socket uticnica, AtomicBoolean status, AtomicBoolean kraj,
+      AtomicBoolean ispis, AtomicInteger brojacUdaljenosti) {
+    this.uticnica = uticnica;
+
+    this.status = status;
+    this.kraj = kraj;
+    this.ispis = ispis;
+    this.brojacUdaljenosti = brojacUdaljenosti;
   }
 
   @Override
   public void run() {
-    boolean status = true;
-    azurirajStatus.accept(status);
+    // flag.set(!flag.get());
+
+    try {
+
+      var citac = new BufferedReader(
+          new InputStreamReader(this.uticnica.getInputStream(), Charset.forName("UTF-8")));
+      var pisac = new BufferedWriter(
+          new OutputStreamWriter(this.uticnica.getOutputStream(), Charset.forName("UTF-8")));
+
+      var poruka = new StringBuilder();
+
+      while (true) {
+        var red = citac.readLine();
+        if (red == null)
+          break;
+        if (this.ispis.get())
+          System.out.println(red);
+        poruka.append(red);
+      }
+
+      this.uticnica.shutdownInput();
+
+      String odgovor = this.obradiZahtjev(poruka.toString());
+
+      // TODO UKLONIT KASNIJE
+      if (this.ispis.get())
+        System.out.println("Odgovor: " + odgovor);
+
+      pisac.write(odgovor);
+      pisac.flush();
+
+      this.uticnica.shutdownOutput();
+      this.uticnica.close();
+
+
+    } catch (IOException e) {
+      Logger.getGlobal().log(Level.SEVERE, e.getMessage());
+    }
+  }
+
+  private String obradiZahtjev(String zahtjev) {
+
+    String regex1 = "(STATUS)$";
+    String regex2 = "(KRAJ)$";
+    String regex3 = "(INIT)$";
+    String regex4 = "(PAUZA)$";
+    String regex5 = "(INFO) (DA|NE)$";
+    String regex6 =
+        "(UDALJENOST) ((\\d*\\.)?\\d+) ((\\d*\\.)?\\d+) ((\\d*\\.)?\\d+) ((\\d*\\.)?\\d+)$";
+
+    if (provjeriIzraz(zahtjev, regex1)) { // STATUS
+      if (this.status.get())
+        return "OK 1";
+      else
+        return "OK 0";
+    }
+
+    if (provjeriIzraz(zahtjev, regex2)) { // KRAJ
+      this.kraj.set(true);
+      return "OK";
+    }
+
+    if (provjeriIzraz(zahtjev, regex3)) { // INIT
+      if (!this.status.get()) { // ako je pauziran
+        this.status.set(true);
+        this.brojacUdaljenosti.set(0);
+        return "OK";
+      } else // ako je vec aktivan
+        return "ERROR 02 Poslužitelj je već aktivan";
+    }
+
+    if (provjeriIzraz(zahtjev, regex4)) { // PAUZA
+      if (this.status.get()) { // ako je aktivan
+        this.status.set(false);
+        return "OK" + brojacUdaljenosti.get();
+      } else { // ako je vec pauziran
+        return "ERROR 01 Poslužitelj je pauziran";
+      }
+    }
+
+    if (provjeriIzraz(zahtjev, regex5)) { // INFO
+      var naredba = razdvojiIzraz(zahtjev, regex5);
+
+      if (this.status.get()) { // ako je aktivan
+        if (naredba[1].equals("DA") && !this.ispis.get()) {
+          this.ispis.set(true);
+          return "OK";
+        }
+        if (naredba[1].equals("DA") && this.ispis.get())
+          return "ERROR 03 Poslužitelj već ispisuje na standardni izlaz";
+        if (naredba[1].equals("NE") && this.ispis.get()) {
+          this.ispis.set(false);
+          return "OK";
+        }
+        if (naredba[1].equals("NE") && !this.ispis.get())
+          return "ERROR 04 Poslužitelj već ne ispisuje na standardni izlaz";
+      } else { // ako nije aktivan
+        return "ERROR 01 Poslužitelj je pauziran";
+      }
+    }
+
+    if (provjeriIzraz(zahtjev, regex6)) { // UDALJENOST
+
+    }
+
+    return "ERROR 05 Nepoznata naredba";
+  }
+
+  /**
+   * Provjerava korektnost izraza korištenjem dozvoljenih izraza (eng. Regular expression)
+   * 
+   * @param string Izraz koji se provjerava
+   * @param regex Regularni izraz s kojim se provjerava
+   * @return Vraća true ako je u redu, inače false
+   */
+  private boolean provjeriIzraz(String string, String regex) {
+    String s = string.trim();
+
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(s);
+
+    boolean status = matcher.matches();
+
+    return status;
+  }
+
+  /**
+   * Razdvaja izraz po grupama korištenjem dozvoljenih izraza (eng. Regular expression)
+   * 
+   * @param string Izraz koji se provjerava
+   * @param regex Regularni izraz s kojim se provjerava
+   * @return Vraća polje s razdvojenim dijelovima izraza
+   */
+  private String[] razdvojiIzraz(String string, String regex) {
+    List<String> rezultat = new ArrayList<>();
+
+    String s = string.trim();
+
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(s);
+
+    boolean status = matcher.matches();
+
+    if (status) {
+      int poc = 0;
+      int kraj = matcher.groupCount();
+      for (int i = poc; i <= kraj; i++) {
+        rezultat.add(matcher.group(i));
+      }
+    } else {
+      rezultat = null;
+    }
+
+    return rezultat.toArray(new String[rezultat.size()]);
   }
 
 }
